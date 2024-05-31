dadcd113efb9324bd8fce7d82b51e7d901101272
artifyfun <artifyfun@gmail.com>
2024-05-31 23:38:38 +0800

    fix: app.vue

--- src/App.vue
+++ src/App.vue
@@ -37,6 +37,7 @@ import { ref } from 'vue'
 import { useDark, useToggle } from '@vueuse/core'
 import { BulbOutlined, SyncOutlined } from '@ant-design/icons-vue';
 import { theme, ConfigProvider as AConfigProvider, App as AApp, Divider as ADivider, FloatButtonGroup as AFloatButtonGroup, FloatButton as AFloatButton, BackTop as ABackTop } from 'ant-design-vue'
+
 import PokemonCard from './lib/components/PokemonCard'
 import ImageUploader from './lib/components/ImageUploader/index.vue'
 import ImageCropUploader from './lib/components/ImageCropUploader/index.vue'
@@ -45,7 +46,9 @@ import DeepChat from './lib/components/DeepChat/index.vue'
 // import '../dist/style.css'
 // import {
 //   PokemonCard,
-//   ImageUploader
+//   ImageUploader,
+//   ImageCropUploader,
+//   // DeepChat
 // } from '../dist/index.js'
 
 const cardActive = ref(false)
